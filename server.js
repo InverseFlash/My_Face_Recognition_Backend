9562e25a3cab1ec2342b046f73036df098ac23b7
Justin <45086244+InverseFlash@users.noreply.github.com>
2021-08-03 12:49:28 -0400

    Initial upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+const knex = require("knex");
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "smart-brain",
+  },
+});
+
+app.post("/signin", (req, res) => signin.handleSignIn(req, res, db, bcrypt));
+
+app.post("/register", (req, res) =>
+  register.handleRegister(req, res, db, bcrypt)
+);
+
+app.get("/profile/:id", (req, res) => profile.handleProfile(req, res, db));
+
+app
+  .put("/image", (req, res) => image.handleImage(req, res, db))
+  .post("/imageUrl", (req, res) => image.handleApiCall(req, res));
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
