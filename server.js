5c888718bfe9f3b4a2209ac5645a22718f26d6cb
Justin <45086244+InverseFlash@users.noreply.github.com>
2021-08-03 15:53:04 -0400

    change database property to comply with heroku naming

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors());
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DATABASE_URL || process.env.DB_HOST,
+    connectionString: process.env.DATABASE_URL,
     ssl: true
   },
 });
