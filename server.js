dfc69ea4608ed8563430f2df1723fe55b21bc961
Justin <45086244+InverseFlash@users.noreply.github.com>
2021-10-02 13:53:20 -0400

    update packages

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-app.options("*", cors());
 
 const db = knex({
   client: "pg",
